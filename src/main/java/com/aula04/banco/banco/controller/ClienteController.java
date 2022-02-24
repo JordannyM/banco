2fc6563e5e7d19d340c1e11c3a906a98d0daf57b
JordannyM <jordanny.mizukoshi@gmail.com>
2022-02-23 21:12:52 -0300

    Put and Delete Mapping

--- src/main/java/com/aula04/banco/banco/controller/ClienteController.java
+++ src/main/java/com/aula04/banco/banco/controller/ClienteController.java
@@ -6,12 +6,17 @@ import com.aula04.banco.banco.model.BancoCliente;
 import com.aula04.banco.banco.model.Cliente;
 import com.aula04.banco.banco.model.Conta;
 import com.aula04.banco.banco.model.TipoConta;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+
+import static javax.swing.text.html.HTML.Tag.U;
 
 @RestController
 @RequestMapping("/cliente")
@@ -25,7 +30,9 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseCliente cadastraCliente(@RequestBody RequestCliente requestCliente){
+    public ResponseEntity<ResponseCliente> cadastraCliente(
+            @RequestBody RequestCliente requestCliente,
+            UriComponentsBuilder uriComponentsBuilder){
         List<Conta> contas= new ArrayList<>();
         Conta conta = new Conta(UUID.randomUUID(),random.nextInt(), 23, TipoConta.CONTA_CORRENTE);
         contas.add(conta);
@@ -38,6 +45,30 @@ public class ClienteController {
                 contas
         );
         bancoCliente.adiciona(cliente);
-        return new ResponseCliente(cliente);
+
+        URI uri = uriComponentsBuilder.path("/cliente/{id}").buildAndExpand(cliente.getId()).toUri();
+        return ResponseEntity.created(uri).body(new ResponseCliente(cliente));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseCliente> detalhesCliente(@PathVariable UUID id) throws Exception{
+        Cliente cliente = bancoCliente.detalhesCliente(id);
+        return ResponseEntity.ok(new ResponseCliente(cliente));
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<ResponseCliente> atualizaCliente(
+            @PathVariable UUID id,
+            @RequestBody RequestCliente requestCliente) throws Exception {
+
+        Cliente cliente = bancoCliente.atualizaCliente(id, requestCliente);
+
+        return ResponseEntity.ok(new ResponseCliente(cliente));
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<ResponseCliente> deletaCliente(
+            @PathVariable UUID id) throws Exception {
+
+        Cliente cliente = bancoCliente.deletaCliente(id);
+
+        return ResponseEntity.ok(new ResponseCliente(cliente));
     }
 }
--- src/main/java/com/aula04/banco/banco/model/BancoCliente.java
+++ src/main/java/com/aula04/banco/banco/model/BancoCliente.java
@@ -1,7 +1,8 @@
 package com.aula04.banco.banco.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.aula04.banco.banco.dto.RequestCliente;
+
+import java.util.*;
 
 public class BancoCliente {
     private static List<Cliente> clientes = new ArrayList<>();
@@ -12,5 +13,34 @@ public class BancoCliente {
 
         public static List<Cliente> buscar(){
             return clientes;
+        }
+
+        public Cliente detalhesCliente(UUID id) throws Exception{
+            Optional<Cliente> resultCliente =
+            BancoCliente.clientes.stream().filter(cliente ->
+                    Objects.equals(cliente.getId(),id)).findAny();
+            if(resultCliente.isPresent()){
+                return resultCliente.get();
+            } else {
+                throw new Exception("Usuário não encontrado");
+            }
+        }
+
+        public Cliente atualizaCliente(UUID id, RequestCliente requestCliente) throws Exception{
+            BancoCliente.clientes.stream().filter(cliente ->
+                    Objects.equals(cliente.getId(),id))
+                    .forEach(cliente -> {
+                        cliente.setNome(requestCliente.getNome());
+                        cliente.setEmail(requestCliente.getEmail());
+                        cliente.setSenha(requestCliente.getSenha());
+                    });
+            return detalhesCliente(id);
+        }
+
+        public Cliente deletaCliente(UUID id) throws Exception{
+            clientes.remove(BancoCliente.clientes.stream().filter(cliente ->
+                    Objects.equals(cliente.getId(),id)));
+
+            return detalhesCliente(id);
         }
 }
