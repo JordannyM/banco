25a4fd790016999c038cbe94f15b4ba7f3310b51
JordannyM <jordanny.mizukoshi@gmail.com>
2022-02-23 21:59:03 -0300

    Put and Delete Mapping

---
+++ src/main/java/com/aula04/banco/banco/dto/RequestDeposito.java
@@ -0,0 +1,12 @@
+package com.aula04.banco.banco.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter @Setter
+public class RequestDeposito {
+    private double deposito;
+    private UUID conta;
+}
